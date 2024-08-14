9acd2f6a070508b797aa6ac89ed4f3edf29a8448
Miranda <mirandacallejonh@gmail.com>
2024-08-14 19:50:59 +0200

    Bolc 1.7 acabat

--- Bloc1-7.js
+++ Bloc1-7.js
@@ -2,65 +2,79 @@
 
 // Exercici 1 i 2
 
-let promesa = new Promise((resolve, reject) => {
-  setTimeout(() => { resolve('Hola, món') }, 2000) });
+const promesa = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Hola, món");
+  }, 2000);
+});
 
-promesa.then( (value) => { console.log(value) } )
-console.log(promesa);
-
-// Exercici 3
-
-let input = "Hola";
-let promesa3 = new Promise((resolve, reject) => {
-  if (input == "Hola") {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  } else {
-    reject('rejected');
-  }
+promesa.then((value) => {
+  console.log(value);
 });
 
 
-promesa3.then((value) => { console.log(value) }, 
-              (value) => { console.log(value) });
+// Exercici 3
 
-console.log(promesa3);
+function promesa3(input) {
+  return new Promise((resolve, reject) => {
+    if (input == "Hola") {
+      setTimeout(() => {
+        resolve("resolved");
+      }, 2000);
+    } else {
+      reject("rejected");
+    }
+  });
+}
+
+promesa3("Hola").then(
+  (value) => {
+    console.log("promesa3", value);
+  },
+  (value) => {
+    console.log("promesa3 ", value);
+  }
+);
+
 
 //Exercici 4
 
 async function myAsynic() {
-  const result = await promesa();
-  console.log('await:', result);
+  const result = await promesa;
+  console.log("await:", result);
 }
 
 myAsynic();
 
 
-/*
+// Exercici 5
+
+async function myAsynic2() {
+  try {
+    const result = await promesa;
+    console.log("try-await: ", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+myAsynic2();
 
 
+// Exercici 6
 
-Exercici 3
-Promesa amb reject: Crea una promesa que es resolgui després de 2 segons
- si l'input és igual a 'Hola',
- i que la rebutgi si l'input és qualsevol altra cosa.
+const promesaA = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promesa A resolta");
+  }, 2000);
+});
 
+const promesaB = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promesa B resolta");
+  }, 3000);
+});
 
-Exercici 4
-Ús de async/await: Escriu una funció asíncrona que utilitzi la funció await 
-per a esperar el resultat de la promesa creada a l'exercici 1, 
-i que després imprimeixi aquest resultat a la consola.
-
-Nivell 2
-
-
-Exercici 5
-Gestió d'errors amb async/await: Modifica la funció de l'exercici 4
- per a que capturi qualsevol possible error utilitzant un bloc try/catch.
-
-Nivell 3
-Exercici 6
-Promise.all: Crea dues promeses que es resolguin després de 2 i 3 segons, 
-respectivament. Utilitza Promise.all per a esperar que ambdues promeses es resolguin, 
-i imprimeix els resultats a la consola.*/
+Promise.all([promesaA, promesaB])
+  .then((value) => console.log(value))
+  .catch((error) => console.error(error));
